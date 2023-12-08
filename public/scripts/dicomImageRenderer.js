2aba79eaf8e138fbf79432f4377e111491f14a97
sayoojcyriac <sayooj.cyriac@gmail.com>
2023-12-07 23:33:31 -0600

    Scroll images

--- public/scripts/dicomImageRenderer.js
+++ public/scripts/dicomImageRenderer.js
@@ -25,25 +25,73 @@ function initializeCornerstone() {
 // Call this function when your application initializes
 initializeCornerstone();
 
+function setupStackScroll(element, imageIds) {
+  // Initialize stack state
+  const stack = {
+    currentImageIdIndex: 0,
+    imageIds: imageIds,
+  };
+
+  cornerstoneTools.addStackStateManager(element, ["stack"]);
+  cornerstoneTools.addToolState(element, "stack", stack);
+
+  // Activate the stack scroll tool
+  cornerstoneTools.stackScroll.activate(element, 1); // 1 is the left mouse button
+  cornerstoneTools.stackScrollWheel.activate(element);
+}
+
 function renderDICOMImage(file) {
   const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
   const element = document.getElementById("dicomImage");
 
   cornerstone.enable(element);
 
-  cornerstone.loadImage(imageId).then(function (image) {
-    cornerstone.displayImage(element, image);
+  cornerstone
+    .loadImage(imageId)
+    .then(function (image) {
+      cornerstone.displayImage(element, image);
 
-    if (image.data.string("x00280008")) {
-      cornerstoneTools.addStackStateManager(element, ["stack"]);
-      cornerstoneTools.addToolState(element, "stack", {
-        currentImageIdIndex: 0,
-        imageIds: [imageId],
-      });
+      // Check if it's a multi-frame image
+      if (image.data.intString("x00280008")) {
+        const numFrames = image.data.intString("x00280008");
+        let imageIds = Array.from({ length: numFrames }, (_, i) =>
+          cornerstoneWADOImageLoader.wadouri.fileManager.add(file, i)
+        );
 
-      const stackScroll = cornerstoneTools.StackScrollTool;
-      cornerstoneTools.addTool(stackScroll);
-      cornerstoneTools.setToolActive("StackScroll", { mouseButtonMask: 1 });
+        setupStackScroll(element, imageIds);
+
+        initializeScrollOnMouseMove(element);
+      }
+    })
+    .catch(function (error) {
+      console.error("Error loading the image:", error);
+    });
+}
+
+function initializeScrollOnMouseMove(element) {
+  let lastY = 0;
+
+  element.addEventListener("mousemove", function (event) {
+    if (!event.buttons) {
+      return; // Only proceed if the mouse button is pressed
     }
+
+    const deltaY = event.clientY - lastY;
+    lastY = event.clientY;
+
+    const stackState = cornerstoneTools.getToolState(element, "stack");
+    if (stackState) {
+      const stackData = stackState.data[0];
+      const newImageIdIndex =
+        stackData.currentImageIdIndex + (deltaY > 0 ? 1 : -1);
+
+      if (newImageIdIndex >= 0 && newImageIdIndex < stackData.imageIds.length) {
+        cornerstoneTools.scrollToIndex(element, newImageIdIndex);
+      }
+    }
+  });
+
+  element.addEventListener("mousedown", function (event) {
+    lastY = event.clientY;
   });
 }
