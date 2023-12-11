987f56fd2fabdd266e1593555b6982c95d63042e
sayoojcyriac <sayooj.cyriac@gmail.com>
2023-12-11 11:35:53 -0600

    Added pixel data insights

--- public/scripts/dicomReader.js
+++ public/scripts/dicomReader.js
@@ -80,9 +80,70 @@ function readDICOMFile() {
     displayMetadata(dataSet);
 
     renderDICOMImage(file);
+
+    // Display pixel data insights
+    displayPixelDataInsights(dataSet);
   };
 
   reader.readAsArrayBuffer(file);
+}
+
+function displayPixelDataInsights(dataSet) {
+  const insightsDiv = document.getElementById("pixelDataInsights");
+  insightsDiv.innerHTML = ""; // Clear previous insights
+
+  const transferSyntaxUID = dataSet.string("x00020010");
+  let isCompressed = isDataCompressed(transferSyntaxUID);
+  // Display isCompressed insight
+  const compressionStatus = document.createElement("div");
+  compressionStatus.className = "insight";
+  compressionStatus.textContent = `Pixel Data is ${
+    isCompressed ? "Compressed" : "Uncompressed"
+  } - But, double-check the TransferSyntax`;
+  insightsDiv.appendChild(compressionStatus);
+
+  // Get relevant attributes to compute expected pixel data length
+  const rows = dataSet.uint16("x00280010");
+  const columns = dataSet.uint16("x00280011");
+  const samplesPerPixel = dataSet.uint16("x00280002");
+  const bitsAllocated = dataSet.uint16("x00280100");
+  const numberOfFrames = dataSet.intString("x00280008") || 1;
+  const photometricInterpretation = dataSet.string("x00280004");
+
+  let bytesPerPixel = bitsAllocated / 8;
+  let exptectedPixelDataLength =
+    rows * columns * bytesPerPixel * samplesPerPixel * numberOfFrames;
+
+  if (photometricInterpretation === "YBR_FULL_422") {
+    // Adjust for YBR_FULL_422 chroma subsampling
+    exptectedPixelDataLength = rows * columns * bytesPerPixel * numberOfFrames;
+    exptectedPixelDataLength +=
+      rows * Math.ceil(columns / 2) * bytesPerPixel * numberOfFrames;
+  }
+
+  // Expected Pixel Data Length
+  const expectedLengthDiv = document.createElement("div");
+  expectedLengthDiv.className = "insight";
+  expectedLengthDiv.textContent = `Expected Pixel Data Length: ${exptectedPixelDataLength} bytes`;
+  insightsDiv.appendChild(expectedLengthDiv);
+
+  // Raw Pixel Data Length
+  const rawPixelDataLength = dataSet.elements.x7fe00010.length; // Raw data length
+  const actualLengthDiv = document.createElement("div");
+  actualLengthDiv.className = "insight";
+  actualLengthDiv.textContent = `Raw Pixel Data Length in DICOM: ${rawPixelDataLength} bytes`;
+  insightsDiv.appendChild(actualLengthDiv);
+}
+
+function isDataCompressed(transferSyntaxUID) {
+  // Common uncompressed Transfer Syntax UIDs
+  const uncompressedSyntaxUIDs = [
+    "1.2.840.10008.1.2", // Implicit VR Little Endian
+    "1.2.840.10008.1.2.1", // Explicit VR Little Endian
+    "1.2.840.10008.1.2.2", // Explicit VR Big Endian
+  ];
+
+  return !uncompressedSyntaxUIDs.includes(transferSyntaxUID);
 }
 
 document
